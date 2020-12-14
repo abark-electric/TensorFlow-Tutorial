983e03594fceefdb5d9b3916fe998d6bce8e0052
Your Name <code.abrarzawad@gmail.com>
2020-12-13 22:24:34 -0500

    Downloaded model and label

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""Download test images"""
+"""Object Detection From TF1 Saved Model"""
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
@@ -25,4 +25,28 @@ def download_images():
     return image_paths
 
 
-IMAGE_PATHS = download_images()
+# IMAGE_PATHS = download_images()
+
+
+def download_model(model_name):
+    base_url = 'http://download.tensorflow.org/models/object_detection/'
+    model_file = f'{model_name}.tar.gz'
+    model_dir = tf.keras.utils.get_file(fname=model_name, origin=base_url+model_file, untar=True)
+
+    return str(model_dir)
+
+
+# MODEL_NAME = 'ssd_mobilenet_v2_coco_2018_03_29'
+# PATH_TO_MODEL_DIR = download_model(MODEL_NAME)
+
+
+def download_labels(filename):
+    base_url = 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/data/'
+    label_dir = tf.keras.utils.get_file(fname=filename, origin=base_url+filename, untar=False)
+    label_dir = pathlib.Path(label_dir)
+
+    return str(label_dir)
+
+
+# LABEL_FILENAME = 'mscoco_label_map.pbtxt'
+# PATH_TO_LABELS = download_labels(LABEL_FILENAME)
